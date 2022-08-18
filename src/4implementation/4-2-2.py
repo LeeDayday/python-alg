40993a1dcc5e9dcca4ac1f71204d3caea5ce428f
LeeDayday <dayoon0905@gmail.com>
2022-08-19 00:23:53 +0900

    4-2-2 수정

--- src/4implementation/4-2-2.py
+++ src/4implementation/4-2-2.py
@@ -9,7 +9,7 @@
 h = int(input())
 
 count = 0
-for i in range(n+1): # hour
+for i in range(h+1): # hour
     for j in range(60): # min
         for k in range(60): #sec
             # 매 시각 안에 '3'이 포함되어 있다면 카운트 증가
