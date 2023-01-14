0a6cc1038b52a4afb8202fd8ea1064c47581db06
LeeDayday <dayoon0905@gmail.com>
2023-01-14 22:36:00 +0900

    6-8 실전 문제 추가

---
+++ src/6sorting/6-8.py
@@ -0,0 +1,21 @@
+# 실전 6-8 두 배열의 원소 교체
+
+     
+# =======================================
+# 결론: A의 최솟값과 B의 최댓값 swap
+
+n, k = map(int, input().split())
+
+listA = list(map(int, input().split()))
+listB = list(map(int, input().split()))
+
+listA.sort()
+listB.sort(reverse=True)
+
+for i in range(k):
+    if listA[i] < listB[i]:
+        listA[i], listB[i] = listB[i], listA[i]
+    else:
+        break
+
+print(listA)
